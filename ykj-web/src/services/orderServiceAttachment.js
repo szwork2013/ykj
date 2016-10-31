3fee4e7cc758b4286fd6a2ea4afb7ec8d277c74c
JunLin.Yang <648795850@qq.com>
2016-10-31 09:02:20 +0800

    功能修改新增

--- ykj-web/src/services/orderServiceAttachment.js
+++ ykj-web/src/services/orderServiceAttachment.js
@@ -1,42 +1,6 @@
 import request from '../utils/request';
 import querystring from 'querystring';
 
-/**
- * 根据姓名搜索用户信息
- */
-export async function searchCustomers(access_token, name) {
-	console.log("111"+name)
-  return request(`/api/customers/searchCustomersAllByName/${name}`, {
-    headers: {
-      'Authorization': `Bearer ${access_token}`,
-      'Content-Type': 'application/json',
-    },
-  })
-}
-
-/**
- * 根据类型获取词典数据信息
- */
-export async function searchCodewordsByType(access_token, query) {
-  return request(`/api/codeWords?${querystring.stringify(query)}`, {
-    headers: {
-      'Authorization': `Bearer ${access_token}`,
-      'Content-Type': 'application/json',
-    },
-  });
-}
-
-/**
- * 根据组织父节点获取其下的组织及人员信息
- */
-export async function searchSubOfficesAndClerks(access_token, parentId) {
-  return request(`/api/offices/${parentId}/searchUnderOfficesAndClerks`, {
-    headers: {
-      'Authorization': `Bearer ${access_token}`,
-      'Content-Type': 'application/json',
-    },
-  });
-}
 
 /**
  * 服务单附件查询
